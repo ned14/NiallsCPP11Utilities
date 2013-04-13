9e1d8984c90f892dfdf80671e862abdd6e48c3f0
ned <ned@kate.nedland>
2013-04-13 00:19:50 -0400

    Added missing std::hash overload.

--- std_filesystem.hpp
+++ std_filesystem.hpp
@@ -4,6 +4,8 @@ Gets a std::filesystem implementation from somewhere
 File Created: Mar 2013
 */
 
+#ifndef STD_FILESYSTEM_FROM_SOMEWHERE
+#define STD_FILESYSTEM_FROM_SOMEWHERE
 #if __cplusplus > 201103L
 #include <filesystem>
 #elif defined(HAVE_TR2_FILESYSTEM)
@@ -11,5 +13,16 @@ File Created: Mar 2013
 namespace std { namespace filesystem { using namespace tr2::filesystem; } }
 #else
 #include "boost/filesystem/convenience.hpp"
-namespace std { namespace filesystem { using namespace boost::filesystem; using boost::filesystem::path; } }
+namespace std {
+	namespace filesystem { using namespace boost::filesystem; using boost::filesystem::path; }
+	template<class T> struct hash;
+	template<> struct hash<boost::filesystem::path>
+	{
+		size_t operator()(const boost::filesystem::path& p) const
+		{
+			return boost::filesystem::hash_value(p);
+		}
+	};
+}
+#endif
 #endif
